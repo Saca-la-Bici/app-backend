7faa5a6c3eaea708078cea6b9b6afe8e6a638229
Jesus0204 <a01705442@tec.mx>
2024-08-29 14:51:06 -0600

    docs(Architecture): Terminar modularizacion

---
+++ modules/anuncios/controllers/registrarAnuncio.controller.js
@@ -0,0 +1,3 @@
+exports.getPrueba = (request, response) => {
+    response.json('Anuncio: Esta es la plantilla que deben usar.');
+};
---
+++ modules/estadisticas/controllers/consultarEstadisticas.controller.js
@@ -0,0 +1,3 @@
+exports.getPrueba = (request, response) => {
+    response.json('Estadisticas: Esta es la plantilla que deben usar.');
+};
