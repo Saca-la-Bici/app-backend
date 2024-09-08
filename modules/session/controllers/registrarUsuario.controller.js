e3f527149d1c8512bb09d1d153b2b91d02a56ea3
Jesus0204 <a01705442@tec.mx>
2024-09-08 12:10:24 -0600

    feat: Agregar Rol a Usuario

--- modules/session/controllers/registrarUsuario.controller.js
+++ modules/session/controllers/registrarUsuario.controller.js
@@ -1,5 +1,7 @@
 // const admin = require('../../../util/firebase');
 const Usuario = require('../../../models/perfil/usuario.model');
+const Rol = require('../../../models/perfil/rol.model');
+const PoseeRol = require('../../../models/perfil/poseeRol.model'); // Modelo para la relación entre usuario y rol
 
 exports.registrarUsuario = async (request, response) => {
     try {
@@ -12,6 +14,16 @@ exports.registrarUsuario = async (request, response) => {
             correoElectronico, 
             numeroEmergencia
         } = request.body;
+
+        // Buscar el rol Usuario
+        const rolUsuario = await Rol.findOne({
+            nombre: 'Usuario'
+        });
+        if (!rolUsuario) {
+            return response.status(404).json({
+                message: 'Rol Usuario no encontrado'
+            });
+        }
 
         // Crear el usuario en MongoDB, por ahora sin firebaseUID
         const newUser = new Usuario({
@@ -26,8 +38,19 @@ exports.registrarUsuario = async (request, response) => {
 
         await newUser.save();
 
+        // Poblar la tabla Posee con la relación entre el usuario y el rol
+        const poseeRol = new PoseeRol({
+            // ObjectId del nuevo usuario
+            IDUsuario: newUser._id, 
+            // ObjectId del rol "Usuario"
+            IDRol: rolUsuario._id 
+        });
+
+        // Guardar la relación en la tabla Posee
+        await poseeRol.save();
+
         response.status(201).json({
-            message: 'User registered successfully',
+            message: 'User registrado con éxito',
             user: newUser
         });
 
