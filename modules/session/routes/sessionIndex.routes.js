376efc8f4a24f780fc1b1870bdbee93e60d59ff2
Jesus0204 <a01705442@tec.mx>
2024-09-08 11:58:16 -0600

    feat: Agregar endpoints para Rol y Privilegios

--- modules/session/routes/sessionIndex.routes.js
+++ modules/session/routes/sessionIndex.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const registrarUsuarioRoute = require('./registrarUsuario.routes');
+const rolPrivilegioRoute = require('./rolPrivilegio.routes');
 
 router.use('/registrarUsuario', registrarUsuarioRoute);
+router.use('/rolPrivilegio', rolPrivilegioRoute);
 
 module.exports = router;
