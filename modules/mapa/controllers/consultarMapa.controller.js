9dbb03f8f8c4e8da9e65c43df2abeebd23fad539
Zathiald <A01705403@tec.mx>
2024-09-09 12:58:33 -0600

    Feat: Funcion consultar rutas

--- modules/mapa/controllers/consultarMapa.controller.js
+++ modules/mapa/controllers/consultarMapa.controller.js
@@ -1,3 +1,10 @@
-exports.getPrueba = (request, response) => {
-    response.json('Mapa: Esta es la plantilla que deben usar.');
+const Ruta = require('../../../models/ruta/ruta.model');
+
+exports.getRutas = async (req, res) => {
+    try {
+        const rutas = await Ruta.find({})  
+        res.json(rutas);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener las rutas', error });
+    }
 };
