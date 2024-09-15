08eab4a1e76eb1b84baf7ba13eb9d888107855bc
AndreaMedinaRico <a01705541@tec.mx>
2024-09-14 18:39:03 -0600

    refactor(actividades): registrar por rodada, evento o taller

--- modules/actividades/controllers/registrarActividad.controller.js
+++ modules/actividades/controllers/registrarActividad.controller.js
@@ -18,10 +18,50 @@ exports.postRegistrarRodada = (request, response) => {
     } = request.body;
 
     try {
-        Actividad.registrarActividad(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen, ruta);
-        res.status(201).json(nuevaRodada);
+        Rodada.registrarRodada(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen, ruta);
+        res.status(201).json({ message: 'Rodada creada exitosamente.', error });
     } catch (error) {
         res.status(500).json({ message: 'Error al crear la rodada', error });
     }
 
+}
+
+exports.postRegistrarTaller = (request, response) => {
+    const {
+        titulo,
+        fechaHora,
+        personasInscritas,
+        ubicacion,
+        descripcion,
+        estado,
+        duracion,
+        imagen
+    } = request.body;
+
+    try {
+        Taller.registrarTaller(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen);
+        res.status(201).json({ message: 'Taller creado exitosamente.', error });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear el taller', error });
+    }
+}
+
+exports.postRegistrarEvento = (request, response) => {
+    const {
+        titulo,
+        fechaHora,
+        personasInscritas,
+        ubicacion,
+        descripcion,
+        estado,
+        duracion,
+        imagen
+    } = request.body;
+
+    try {
+        Evento.registrarEvento(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen);
+        res.status(201).json({ message: 'Evento creado exitosamente.', error });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear el evento', error });
+    }
 }
