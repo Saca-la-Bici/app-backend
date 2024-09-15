337675c098dd94abfe77623bd48037ed24ac4f47
SebastianCo1126 <colin.sebas02@gmail.com>
2024-09-15 15:17:27 -0600

    feat(actividades): HU Modificar Actividad, modelo, controlador y rutas.

---
+++ modules/actividades/controllers/modificarActividad.controller.js
@@ -0,0 +1,13 @@
+const { modificarEvento } = requestuire('../../../models/actividades/registraActividad.model.js');
+
+exports.postModificarActividad = async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+
+    try {
+        const updatedActivity = await modificarEvento(id, data);
+        res.status(201).json({ message: 'Actividad modificada exitosamente.', updatedActivity });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al modificar la actividad', error });
+    }
+};
