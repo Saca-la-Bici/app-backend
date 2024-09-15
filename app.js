36bcc24fc5ab9514dbd8249e1c8baf2b929ab522
Tony <111481792+Bowist27@users.noreply.github.com>
2024-09-15 17:38:30 -0600

    fix: Usar Conexion EC2

--- app.js
+++ app.js
@@ -23,9 +23,10 @@ const compression = require('compression');
 app.use(compression());
 
 // Conectar a la base de datos local de MongoDB
-mongoose.connect('mongodb://localhost:27017/Saca_la_Bici')
+mongoose.connect('mongodb://lec2-3-144-225-207.us-east-2.compute.amazonaws.com:27017/Saca_la_Bici')
+
 .then(() => {
-    console.log('Conectado a la base de datos local de MongoDB');
+    console.log('Conectado a la base de datos de MongoDB en AWS EC2');
 }).catch((error) => {
     console.error('Error al conectar con la base de datos:', error);
 });
