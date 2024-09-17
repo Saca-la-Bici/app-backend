dc61c288cf7767fe48effe2fdcb87b2264002ef3
Jesus0204 <a01705442@tec.mx>
2024-09-16 22:05:10 -0600

    feat: Agregar que el login se pueda con username

--- models/perfil/usuario.model.js
+++ models/perfil/usuario.model.js
@@ -4,6 +4,7 @@ const usuarioSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
     },
     nombre: {
         type: String,
--- modules/session/routes/sessionIndex.routes.js
+++ modules/session/routes/sessionIndex.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const registrarUsuarioRoute = require('./registrarUsuario.routes');
 const rolPrivilegioRoute = require('./rolPrivilegio.routes');
+const getUserEmail = require('./getUserEmail.routes');
 
 router.use('/registrarUsuario', registrarUsuarioRoute);
 router.use('/rolPrivilegio', rolPrivilegioRoute);
+router.use('/getUserEmail', getUserEmail);
 
 module.exports = router;
