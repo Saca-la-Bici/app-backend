34b4b53b9566be1ad09faa723bda9f58f5fcb161
AndreaMedinaRico <a01705541@tec.mx>
2024-09-17 01:20:17 -0600

    refac(actividades): reducir registrarActividad a una función en modelo y controlador

--- models/actividades/registraActividad.model.js
+++ models/actividades/registraActividad.model.js
@@ -2,66 +2,20 @@ const Rodada = require('./rodada.model');
 const Taller = require('./taller.model');
 const Evento = require('./evento.model');
 
-async function registrarRodada(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen, ruta) {
-    try {
-        const rodada = await Rodada.create({
-            titulo: titulo,
-            fechaHora: fechaHora,
-            personasInscritas: personasInscritas,
-            ubicacion: ubicacion,
-            descripcion: descripcion,
-            estado: estado,
-            duracion: duracion,
-            imagen: imagen, 
-            ruta: ruta
-        });
-        await rodada.save();
+async function encontrarTipo(tipo) {
+    if (tipo === 'rodada') return Rodada;
+    if (tipo === 'taller') return Taller;
+    if (tipo === 'evento') return Evento;
+}
 
+async function registrarActividad(tipo, data) {
+    try {
+        const Modelo = await encontrarTipo(tipo);
+        const actividad = await Modelo.create(data);
+        await actividad.save();
     } catch (error) {
         throw(error);
     }
 }
 
-async function registrarTaller(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen) {
-    try {
-        const taller = await Taller.create({
-            titulo: titulo,
-            fechaHora: fechaHora,
-            personasInscritas: personasInscritas,
-            ubicacion: ubicacion,
-            descripcion: descripcion,
-            estado: estado,
-            duracion: duracion,
-            imagen: imagen
-        });
-        await taller.save();
-
-    } catch (error) {
-        throw(error);
-    }
-}
-
-async function registrarEvento(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen) {
-    try {
-        const evento = await Evento.create({
-            titulo: titulo,
-            fechaHora: fechaHora,
-            personasInscritas: personasInscritas,
-            ubicacion: ubicacion,
-            descripcion: descripcion,
-            estado: estado,
-            duracion: duracion,
-            imagen: imagen
-        });
-        await evento.save();
-
-    } catch (error) {
-        throw(error);
-    }
-}
-
-module.exports = {
-    registrarRodada,
-    registrarTaller,
-    registrarEvento
-}
+module.exports = { registrarActividad };
--- modules/actividades/controllers/registrarActividad.controller.js
+++ modules/actividades/controllers/registrarActividad.controller.js
@@ -1,24 +1,15 @@
-const {Rodada, Taller, Evento} = require('../../../models/actividades/registraActividad.model.js');
+const { registrarActividad } = require('../../../models/actividades/registraActividad.model.js');
 
 exports.getRegistrarActividad = (request, response) => {
     response.json('Actividad: Esta es la plantilla que deben usar.');
 };
 
-exports.postRegistrarRodada = (request, response) => {
-    const {
-        titulo,
-        fechaHora,
-        personasInscritas,
-        ubicacion,
-        descripcion,
-        estado,
-        duracion,
-        imagen,
-        ruta
-    } = request.body;
+exports.postRegistrarActividad = (request, response) => {
+    const data = request.body;
+    const tipo = request.params.tipo;
 
     try {
-        Rodada.registrarRodada(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen, ruta);
+        registrarActividad(tipo, data);
         res.status(201).json({ message: 'Rodada creada exitosamente.', error });
     } catch (error) {
         res.status(500).json({ message: 'Error al crear la rodada', error });
@@ -26,42 +17,3 @@ exports.postRegistrarRodada = (request, response) => {
 
 }
 
-exports.postRegistrarTaller = (request, response) => {
-    const {
-        titulo,
-        fechaHora,
-        personasInscritas,
-        ubicacion,
-        descripcion,
-        estado,
-        duracion,
-        imagen
-    } = request.body;
-
-    try {
-        Taller.registrarTaller(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen);
-        res.status(201).json({ message: 'Taller creado exitosamente.', error });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al crear el taller', error });
-    }
-}
-
-exports.postRegistrarEvento = (request, response) => {
-    const {
-        titulo,
-        fechaHora,
-        personasInscritas,
-        ubicacion,
-        descripcion,
-        estado,
-        duracion,
-        imagen
-    } = request.body;
-
-    try {
-        Evento.registrarEvento(titulo, fechaHora, personasInscritas, ubicacion, descripcion, estado, duracion, imagen);
-        res.status(201).json({ message: 'Evento creado exitosamente.', error });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al crear el evento', error });
-    }
-}
