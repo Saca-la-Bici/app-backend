407f9c4f9aeae1dc6fa967d5aa199d6b0cb55404
Juan Pablo <a01705408@tec.mx>
2024-09-18 10:57:29 -0600

    fix: Uso de PUT para modificar una pregunta

--- modules/preguntasFrecuentes/routes/consultarPreguntaFrecuenteIndividual.routes.js
+++ modules/preguntasFrecuentes/routes/consultarPreguntaFrecuenteIndividual.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const helperPregunta = require('../controllers/consultarPreguntaFrecuenteIndividual.controller');
 
 router.get('/:IdPregunta', helperPregunta.get_PreguntaIndividual);
+router.put('/:IdPregunta/Modificacion', helperPregunta.put_modificarPreguntaFrecuente);
 
 module.exports = router;
