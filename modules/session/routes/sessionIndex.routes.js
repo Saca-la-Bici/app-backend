3160ff7acffc9432e5450bb7886827a6f3cdadf9
Jesus0204 <a01705442@tec.mx>
2024-09-17 18:36:59 -0600

    feat: endpoints necesarios para firebase

--- modules/session/routes/sessionIndex.routes.js
+++ modules/session/routes/sessionIndex.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const registrarUsuarioRoute = require('./registrarUsuario.routes');
 const rolPrivilegioRoute = require('./rolPrivilegio.routes');
-const getUserEmail = require('./getUserEmail.routes');
+const getUserEmailRoute = require('./getUserEmail.routes');
+const perfilCompletoRoute = require('./perfilCompleto.routes');
 
 router.use('/registrarUsuario', registrarUsuarioRoute);
 router.use('/rolPrivilegio', rolPrivilegioRoute);
-router.use('/getUserEmail', getUserEmail);
+router.use('/getUserEmail', getUserEmailRoute);
+router.use('/perfilCompleto', perfilCompletoRoute);
 
 module.exports = router;
