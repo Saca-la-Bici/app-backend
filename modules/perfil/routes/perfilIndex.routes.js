c591b5636cef906af3bb711873c818d91ce0cf05
Angeltrek <angeltrek007@gmail.com>
2024-09-22 03:41:38 -0600

    feat(buscarUsuarios): agregar buscar usuarios por nombre o correo

--- modules/perfil/routes/perfilIndex.routes.js
+++ modules/perfil/routes/perfilIndex.routes.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const consultarUsuariosRoute = require('./consultarUsuario.routes');
+const consultarUsuariosRoute = require("./consultarUsuarios.routes");
+const buscarUsuariosRoute = require("./buscarUsuarios.routes");
 
 //Importar y usar las rutas en el enrutador principal del módulo
-router.use('/consultarUsuarios', consultarUsuariosRoute);
+router.use("/consultarUsuarios", consultarUsuariosRoute);
+router.use("/buscarUsuarios", buscarUsuariosRoute);
 
 module.exports = router;
