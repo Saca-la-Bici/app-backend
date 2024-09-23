dfe447ee0b47b6f315fe187a58aab6473cb5e808
AndreaMedinaRico <a01705541@tec.mx>
2024-09-23 11:01:37 -0600

    refac(actividades): registrar rodada y separar funciones por tipo

--- modules/actividades/controllers/registrarActividad.controller.js
+++ modules/actividades/controllers/registrarActividad.controller.js
@@ -1,4 +1,9 @@
-const { registrarActividad } = require('../../../models/actividades/registraActividad.model.js');
+const { 
+    registrarActividad,
+    postRodada,
+    postTaller,
+    postEvento
+} = require('../../../models/actividades/registraActividad.model.js');
 
 exports.getRegistrarActividad = (request, response) => {
     response.json('Actividad: Esta es la plantilla que deben usar.');
@@ -16,6 +21,43 @@ exports.postRegistrarActividad = async (request, response) => {
     } catch (error) {
         response.status(500).json({ message: `Error al crear la ${tipo}`, error });
     }
-
 }
 
+exports.postRodada = async (request, response) => {
+    const data = request.body;
+    const {id} = request.query;
+
+    console.log("Los datos son:", data);
+    console.log("El id de la ruta es:", id);
+
+    try {
+        const registro = await postRodada(data, id);
+        response.status(201).json({ message: `Rodada creada exitosamente.`, registro });
+    } catch (error) {
+        response.status(500).json({ message: `Error al crear la rodada`, error });
+    }
+}
+
+exports.postTaller = async (request, response) => {
+    const data = request.body;
+    console.log("Los datos son:", data);
+
+    try {
+        const registro = await postTaller(data);
+        response.status(201).json({ message: `Taller creado exitosamente.`, registro });
+    } catch (error) {
+        response.status(500).json({ message: `Error al crear el taller`, error });
+    }
+}
+
+exports.postEvento = async (request, response) => {
+    const data = request.body;
+    console.log("Los datos son:", data);
+
+    try {
+        const registro = await postEvento(data);
+        response.status(201).json({ message: `Evento creado exitosamente.`, registro });
+    } catch (error) {
+        response.status(500).json({ message: `Error al crear el evento`, error });
+    }
+}
