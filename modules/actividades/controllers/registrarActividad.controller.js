bb7108e2cf6e4e184fe0eb9c01b9f00ff37ea1dc
AndreaMedinaRico <a01705541@tec.mx>
2024-09-23 11:02:51 -0600

    refactor(actividades): eliminar funciones no utilizadas

--- modules/actividades/controllers/registrarActividad.controller.js
+++ modules/actividades/controllers/registrarActividad.controller.js
@@ -5,24 +5,6 @@ const {
     postEvento
 } = require('../../../models/actividades/registraActividad.model.js');
 
-exports.getRegistrarActividad = (request, response) => {
-    response.json('Actividad: Esta es la plantilla que deben usar.');
-};
-
-exports.postRegistrarActividad = async (request, response) => {
-    const data = request.body;
-    const tipo = request.params.tipo;
-    console.log("El tipo es:", tipo);
-    console.log("Los datos son:", data);
-
-    try {
-        const registro = await registrarActividad(tipo, data);
-        response.status(201).json({ message: `${tipo} creada exitosamente.`, registro });
-    } catch (error) {
-        response.status(500).json({ message: `Error al crear la ${tipo}`, error });
-    }
-}
-
 exports.postRodada = async (request, response) => {
     const data = request.body;
     const {id} = request.query;
--- modules/actividades/routes/registrarActividad.routes.js
+++ modules/actividades/routes/registrarActividad.routes.js
@@ -5,9 +5,6 @@ const router = express.Router();
 const registrarActividadController = require('../controllers/registrarActividad.controller');
 
 // Definir la ruta para crear una actividad
-router.get('/', registrarActividadController.getRegistrarActividad);
-//router.post('/:tipo', registrarActividadController.postRegistrarActividad);
-
 router.post('/rodada', registrarActividadController.postRodada);
 router.post('/taller', registrarActividadController.postTaller);
 router.post('/evento', registrarActividadController.postEvento);
