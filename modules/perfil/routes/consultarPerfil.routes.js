fe94bfac38ba7fbb8b2a09351b3a60c0b25ff36b
Jesus0204 <a01705442@tec.mx>
2024-09-24 20:36:56 -0600

    fix: ruta a verifyUserToke

--- modules/perfil/routes/consultarPerfil.routes.js
+++ modules/perfil/routes/consultarPerfil.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Importar el controlador que maneja la creación de actividades
 const consultarPerfilController = require("../controllers/consultarPerfil.controller");
 
-const verifyToken = require("./util/verifyUserToken");
+const verifyToken = require("../../../util/verifyUserToken");
 
 // Definir la ruta para crear una actividad
 router.get("/", verifyToken, consultarPerfilController.get_Perfil);
