7f984d7dcd7b7ec991536c2084ef41fab2bad72f
DanielContrerasCh <danicontreras0409@gmail.com>
2024-09-25 13:41:04 -0600

    <fix>: Adición de middleware faltante de verificar token en modificarAnuncio

--- modules/anuncios/routes/modificarAnuncio.routes.js
+++ modules/anuncios/routes/modificarAnuncio.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const verifyUserToken = require('../../../util/verifyUserToken');
 
 // Importar el controlador que maneja la creación de actividades
 const modificarAnuncioController = require('../controllers/modificarAnuncio.controller');
 
 // Definir la ruta para crear una actividad
-router.put('/:IDAnuncio', modificarAnuncioController.putAnnouncement);
+router.put('/:IDAnuncio', verifyUserToken, modificarAnuncioController.putAnnouncement);
 
 module.exports = router;
