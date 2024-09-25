02b52d55763302c5b58048c55294c8685b457a3f
Angie <A01705651@tec.mx>
2024-09-24 19:52:00 -0600

    feat(perfil): Consultar perfil

--- modules/perfil/controllers/consultarPerfil.controller.js
+++ modules/perfil/controllers/consultarPerfil.controller.js
@@ -1,19 +1,20 @@
-const Perfil = require('../../../models/perfil/usuario.model');
+const Usuario = require('../../../models/perfil/usuario.model');
 
-exports.consultarPerfil = async (request, response) => {
-    try{
-        const perfil = await Perfil.find();
+exports.get_Perfil = async (request, response) => {
+    try {
+        const { firebaseUID } = request.params; 
+        const perfil = await Usuario.findOne({ firebaseUID });
         return response.status(200).json({
             code: 200,
             msg: 'Consulta de datos de perfil exitosa',
             data: perfil
         });
-    } catch(error){
+    } catch (error) {
         console.error(error);
-        return ressponse.status(500).json({
+        return response.status(500).json({
             code: 500,
             msg: 'Error al intentar consultar el perfil',
             data: null
         });
     }
-}
+};
