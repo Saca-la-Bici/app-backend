3ececbb90418c385815ff0b43ccd0340c4c75676
MariaJoseGaytan <A01706616@tec.mx>
2024-09-25 15:45:34 -0600

    <fix> Cambiar IDUsuario a FirebaseUID

--- modules/anuncios/controllers/modificarAnuncio.controller.js
+++ modules/anuncios/controllers/modificarAnuncio.controller.js
@@ -2,12 +2,12 @@ const Announcement = require('../../../models/otros/anuncio.model');
 
 exports.putAnnouncement = async (request, response) => {
     const IDAnuncio = request.params.IDAnuncio
-    const idUsuario = request.body.IDUsuario;
+    const firebaseUID = request.userUID.uid;
     const titulo = request.body.titulo;
     const contenido = request.body.contenido;
     const imagen = request.body.imagen;
     try {
-        const anuncio = await Announcement.putAnnouncement(IDAnuncio, idUsuario, titulo, contenido, imagen);
+        const anuncio = await Announcement.putAnnouncement(IDAnuncio, firebaseUID, titulo, contenido, imagen);
         return response.status(201).json(anuncio);
     } catch (error) {
         return response.status(404).json({ message: 'Anuncio no encontrado', error: error.message });
