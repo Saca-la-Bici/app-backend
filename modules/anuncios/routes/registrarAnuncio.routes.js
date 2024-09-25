0da9f66861b38e7882dc3c14ada258455e12d8ad
DanielContrerasCh <danicontreras0409@gmail.com>
2024-09-24 21:53:08 -0600

    <refactor>: obtener firebaseUID del request y usar verifytoken

--- modules/anuncios/routes/registrarAnuncio.routes.js
+++ modules/anuncios/routes/registrarAnuncio.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const verifyUserToken = require('../../../util/verifyUserToken');
 
 // Importar el controlador que maneja la creación de actividades
 const registrarAnuncioController = require('../controllers/registrarAnuncio.controller');
 
 // Definir la ruta para crear una actividad
-router.post('/', registrarAnuncioController.postAnnouncement);
+router.post('/', verifyUserToken, registrarAnuncioController.postAnnouncement);
 
 module.exports = router;
