5676266eb7179cf9419efba7750d1bc7af8c9f58
AndreaMedinaRico <a01705541@tec.mx>
2024-09-27 12:34:11 -0600

    fix(actividades): completar verificación de usuario

--- modules/actividades/routes/registrarActividad.routes.js
+++ modules/actividades/routes/registrarActividad.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const verifyUserToken = require('../../../util/verifyUserToken')
+const verifyUserRole = require('../../../util/verifyUserRole');
 
 // Importar el controlador que maneja la creación de actividades
 const registrarActividadController = require('../controllers/registrarActividad.controller');
 
 // Definir la ruta para crear una actividad
-router.post('/rodada', verifyUserToken, registrarActividadController.postRodada);
-router.post('/taller', verifyUserToken, registrarActividadController.postTaller);
-router.post('/evento', verifyUserToken, registrarActividadController.postEvento);
+router.post('/rodada', verifyUserToken, verifyUserRole, registrarActividadController.postRodada);
+router.post('/taller', verifyUserToken, verifyUserRole, registrarActividadController.postTaller);
+router.post('/evento', verifyUserToken, verifyUserRole, registrarActividadController.postEvento);
 
 module.exports = router;
