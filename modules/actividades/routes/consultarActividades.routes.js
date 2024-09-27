fac77f45701bb10ed537bbdfda02f13021923989
AndreaMedinaRico <a01705541@tec.mx>
2024-09-27 11:53:22 -0600

    refactor(actividades): añadir verificación de rol

--- modules/actividades/routes/consultarActividades.routes.js
+++ modules/actividades/routes/consultarActividades.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const verifyUserToken = require('../../../util/verifyUserToken');
+const verifyUserRole = require('../../../util/verifyUserRole');
 
 const { getRodadas, getEventos, getTalleres } = require('../controllers/consultarActividades.controller');
 
-router.get('/rodadas', verifyUserToken, getRodadas);
-router.get('/eventos', verifyUserToken, getEventos);
-router.get('/talleres', verifyUserToken, getTalleres);
+router.get('/rodadas', verifyUserToken, verifyUserRole, getRodadas);
+router.get('/eventos', verifyUserToken, verifyUserRole, getEventos);
+router.get('/talleres', verifyUserToken, verifyUserRole, getTalleres);
 
 module.exports = router;
