44f53f2587dd75e06810979dcf3af20061e4eef7
Ethan Luna <ethanlunacado@gmail.com>
2024-09-27 16:26:28 -0600

    agregue los cambios necesarios para la FireBase

--- modules/perfil/routes/modificarRol.routes.js
+++ modules/perfil/routes/modificarRol.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const verifyToken = require('../../../middlewares/verifyToken');
 
 const modificarRolController = require('../controllers/modificarRol.controller');
 
-router.patch('/:id', modificarRolController.patchRole);
+router.patch('/:id', verifyToken, modificarRolController.patchRole);
 
 module.exports = router;
