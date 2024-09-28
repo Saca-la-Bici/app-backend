895f39dceacea01020546a740a0ef5a8c52ed1b9
AntonioLanderos <juan.alanderosv@outlook.com>
2024-09-28 17:13:53 -0600

    feat add backend publicarComentarios

--- modules/actividades/routes/actividadesIndex.routes.js
+++ modules/actividades/routes/actividadesIndex.routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const registrarActividadRoute = require('./registrarActividad.routes');
 const modificarActividadRoute = require('./modificarActividad.routes');
 const consultarActividadesRoute = require('./consultarActividades.routes');
+const publicarComentarioRoute = require('./publicarComentario.routes');
 
 // Importar y usar las rutas en el enrutador principal del módulo
 router.use('/registrar', registrarActividadRoute);
 router.use('/modificar', modificarActividadRoute);
 router.use('/consultar', consultarActividadesRoute);
+router.use('/comentario', publicarComentarioRoute);
 
 module.exports = router;
