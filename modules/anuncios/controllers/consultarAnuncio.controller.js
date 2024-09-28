b9210094b2cb72eee84e58df6a814fdd63b77f6b
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-09-28 00:23:31 -0600

    <feat> Primeros intentos de S3

--- modules/anuncios/controllers/consultarAnuncio.controller.js
+++ modules/anuncios/controllers/consultarAnuncio.controller.js
@@ -1,14 +1,61 @@
-
 const Announcement = require('../../../models/otros/anuncio.model');
+const AWS = require('aws-sdk');
+const getImage = require('../../../util/getImage');
 
-exports.getAnnouncements = async (request, response) => {
+AWS.config.update({
+    signatureVersion: 'v4',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  });
+  
+  const s3 = new AWS.S3();
+
+  exports.getAnnouncements = async (req, res, next) => {
     try {
-        const anuncio = await Announcement.getAnnouncements();
-        if(anuncio.length === 0){
-            return response.status(204).json({ message: 'No hay anuncios' });
-        }
-        return response.status(200).json(anuncio);
+        // Tu lógica para obtener los anuncios
+        const announcements = await Announcement.getAnnouncements(); // Ejemplo de función
+
+        req.announcements = announcements; // Adjuntar los anuncios al objeto de la solicitud
+        next(); // Proceder al siguiente middleware (getImage)
     } catch (error) {
-        return response.status(404).json({ message: 'Anuncio no encontrado', error: error.message });
+        console.error('Error fetching announcements:', error);
+        res.status(500).json({ message: 'Error fetching announcements', error: error.message });
     }
 };
+
+/*
+exports.getImagen = async (request, response) => {
+    const folderPath = 'Announcements/'; // Replace with your folder path
+    const bucketName = process.env.AWS_BUCKET;
+    
+    const params = {
+        Bucket: bucketName,
+        Prefix: folderPath
+    };
+    
+    try {
+        const data = await s3.listObjectsV2(params).promise();
+        const imageKeys = data.Contents.filter(item => {
+            // Filter based on file extensions (e.g., .jpg, .png, .gif)
+            return item.Key.match(/\.(jpg|jpeg|png|gif)$/);
+        }).map(item => item.Key);
+    
+        const imageUrls = imageKeys.map((key) => {
+            const imageParams = {
+                Bucket: bucketName,
+                Key: key,
+                Expires: 60 * 1 // URL expiration time in seconds (e.g., 1 hour)
+            };
+            const url = s3.getSignedUrl('getObject', imageParams);
+            return {
+                key: key,
+                url: url
+            };
+        });
+    
+        response.status(200).json({ images: imageUrls });
+    } catch (error) {
+        console.error('Error fetching images from S3:', error);
+        response.status(500).json({ message: 'Error fetching images from S3', error: error.message });
+    }
+};*/
