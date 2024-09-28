38bf872ccffd108af756ff8a6c2c683ceedea86a
MonicaMMartinezV <A01710965@tec.mx>
2024-09-28 14:03:56 -0600

    feat: Quitar RBAC en backend de modificar y registrar ruta

--- modules/mapa/routes/registrarRuta.routes.js
+++ modules/mapa/routes/registrarRuta.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const verifyUserToken = require('../../../util/verifyUserToken');
 
 // Importar el controlador que maneja la creación de actividades
 const registrarRutaController = require('../controllers/registrarRuta.controller');
 
 // Definir la ruta para crear una actividad
-router.post('/registrarRuta', verifyUserToken, registrarRutaController.postRuta);
+router.post('/registrarRuta', registrarRutaController.postRuta);
 
 module.exports = router;
