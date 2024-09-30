673edf3fc3edc39da27c400a8e64f21560588af5
AntonioLanderos <juan.alanderosv@outlook.com>
2024-09-30 16:29:42 -0600

    feature(foro): publicar comentario

--- modules/foro/controllers/publicarComentario.controller.js
+++ modules/foro/controllers/publicarComentario.controller.js
@@ -1,30 +1,28 @@
 const Comentario = require('../../../models/foro/comentario.model');
 
-exports.publicarComentario = async (req, res) => {
-    try {
-        const {username, fotoPerfil, contenido} = req.body;
+exports.publicarComentario = async (request, response) => {
+    const username = request.body.username;
+    const fotoPerfil = request.body.fotoPerfil;
+    const contenido = request.body.contenido;
 
+    try {
         if (!contenido || contenido.length < 4 || contenido.length > 500) {
-            return res.status(400).json({
+            return response.status(400).json({
                 code: 400,
                 msg: 'El contenido del comentario es inválido',
                 data: null
             });
         }
 
-        const nuevoComentario = new Comentario({
-            username, 
-            //fotoPerfil,
-            contenido
-        });
-        await nuevoComentario.save();
-        return res.status(200).json({
+        const nuevoComentario = await Comentario.publicarComentario(username, fotoPerfil, contenido);
+      
+        return response.status(200).json({
             code: 200,
             msg: 'Comentario publicado exitosamente',
             data: nuevoComentario
         });
     } catch (error) {
-        return res.status(500).json({
+        return response.status(500).json({
             code: 500,
             msg: 'Error al publicar el comentario',
             error: error,
