24a9058cba1636b2b0cd0ec09db8276a32d827ae
Zathiald <A01705403@tec.mx>
2024-09-30 23:18:09 -0600

    Feat: Agregar funcion para actualizar ubicacion

--- models/actividades/rodada.model.js
+++ models/actividades/rodada.model.js
@@ -3,11 +3,28 @@ const Schema = mongoose.Schema;
 const Ruta = require('../ruta/ruta.model');
 const actividadSchema = require('./actividad.model');
 
+const ubicacionSchema = new mongoose.Schema({
+
+    latitud: {
+      type: Number,
+      required: true
+    },
+  
+    longitud: {
+      type: Number,
+      required: true
+    },
+  })
+  
+
 const rodadaSchema = new Schema ({
     informacion: {
         type: [actividadSchema],
         required: true
     }, 
+    ubicacion: {
+        type: [ubicacionSchema]
+    },
     ruta: [{
         type: Schema.Types.ObjectId,
         ref: 'Ruta',
--- modules/rodadas/routes/iniciarRodada.routes.js
+++ modules/rodadas/routes/iniciarRodada.routes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const iniciarRodadaController = require('../controllers/iniciarRodadaController');
-// Ruta para iniciar la rodada
-router.post('/iniciar', iniciarRodadaController.iniciarRodada);
-// Ruta para recibir y actualizar la ubicación del administrador
-router.post('/ubicacion', iniciarRodadaController.actualizarUbicacion);
-// Ruta opcional para obtener la ubicación actual del administrador
-router.get('/ubicacion', iniciarRodadaController.obtenerUbicacion);
+const iniciarRodadaController = require('../controllers/iniciarRodada.controller');
+
+// Ruta para actualizar la ubicación de una rodada existente
+router.put('/:idRodada', iniciarRodadaController.actualizarUbicacion);
+
 module.exports = router;
