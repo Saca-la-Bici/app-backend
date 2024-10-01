cadd7c3f37ed6092dfda476e32085fdbdd5f1036
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-09-30 18:40:01 -0600

    <fix> Errores al hacer merge

--- modules/anuncios/routes/consultarAnuncio.routes.js
+++ modules/anuncios/routes/consultarAnuncio.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const verifyUserToken = require('../../../util/verifyUserToken');
 const verifyUserPermissions = require('../../../util/verifyUserPermissions');
 const consultarAnuncioController = require('../controllers/consultarAnuncio.controller');
-const getImageFolder = require('../../../util/getImageFolder');
 
 
 router.get('/', verifyUserToken, verifyUserPermissions, consultarAnuncioController.getAnnouncements);
