af72d50c2062aaa4ad4a07c45679b0861c33a071
Zathiald <A01705403@tec.mx>
2024-10-01 11:51:29 -0600

    Fix: Agregar verificaciones de token y permisos

--- modules/rodadas/routes/iniciarRodada.routes.js
+++ modules/rodadas/routes/iniciarRodada.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const verifyUserToken = require('../../../util/verifyUserToken');
+const verifyUserPermissions = require('../../../util/verifyUserPermissions');
 const iniciarRodadaController = require('../controllers/iniciarRodada.controller');
 
 // Ruta para actualizar la ubicación de una rodada existente
-router.put('/:idRodada', iniciarRodadaController.actualizarUbicacion);
+router.put('/:idRodada', verifyUserToken,verifyUserPermissions, iniciarRodadaController.actualizarUbicacion);
 
 module.exports = router;
