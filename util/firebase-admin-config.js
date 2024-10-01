f744a6955ff8602b930858b3e3aa8461bb1173ef
Jesus0204 <a01705442@tec.mx>
2024-10-01 13:34:40 -0600

    hotfix: arreglar el firebase config y el verifyUserToken

--- util/firebase-admin-config.js
+++ util/firebase-admin-config.js
@@ -1,8 +1,8 @@
-/*  const admin = require("firebase-admin");
- const serviceAccount = require("./login-app.json");
+const admin = require("firebase-admin");
+const serviceAccount = require("./login-app.json");
 
- admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount),
- });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
- module.exports = admin; */
+module.exports = admin;
