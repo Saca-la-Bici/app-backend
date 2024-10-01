949ec143228f541cab93eb00f7324905dafec9d2
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-10-01 17:43:15 -0600

    feat: modificar una pregunta frecuente

---
+++ modules/preguntasFrecuentes/routes/modificarPreguntaFrecuente.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const verifyUserToken = require('../../../util/verifyUserToken');
+const verifyUserPermissions = require('../../../util/verifyUserPermissions');
+const { modificarPreguntaFrecuente } = require('../controllers/modificarPreguntaFrecuente.controller');
+
+router.put('/:IdPregunta', verifyUserToken, verifyUserPermissions, modificarPreguntaFrecuente);
+
+module.exports = router;
--- modules/preguntasFrecuentes/routes/preguntasIndex.routes.js
+++ modules/preguntasFrecuentes/routes/preguntasIndex.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const consultarPreguntasRoute = require('./consultarPreguntasFrecuentes.routes');
 const registrarPreguntasRoute = require('./registrarPreguntasFrecuentes.routes');
+const modificarPreguntasRoute = require('./modificarPreguntaFrecuente.routes');
 const consultarPreguntaIndividualRoute = require("./consultarPreguntaFrecuenteIndividual.routes");
 
 router.use('/consultar', consultarPreguntasRoute);  
 router.use('/registrar', registrarPreguntasRoute);  
+router.use('/modificar', modificarPreguntasRoute);
 router.use('/consultarIndividual', consultarPreguntaIndividualRoute);
 
 
