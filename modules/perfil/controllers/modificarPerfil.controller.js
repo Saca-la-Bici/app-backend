18b82159f765d54a315358b358c96ec5b12355a6
Diego <a01705506@tec.mx>
2024-10-01 18:36:25 -0600

    feat(Modificar Perfil) - Backend completo de modificar perfil

--- modules/perfil/controllers/modificarPerfil.controller.js
+++ modules/perfil/controllers/modificarPerfil.controller.js
@@ -1,19 +1,19 @@
-const Perfil = require('../../../models/perfil/usuario.model');
+const Usuario = require('../../../models/perfil/usuario.model');
 
-exports.put_modificarPerfil = async (request, response) => {
-    const IDUsuario = request.body.IDUsuario;
-    const Username = request.body.Username;
-    const nombre = request.body.nombre
-    const tipoSangre = request.body.tipoSangre;
-    const numeroEmergencia = request.body.numeroEmergencia;
 
-    try {
-        const profile = await Perfil.putUsuario(IDUsuario, Username, nombre, tipoSangre, numeroEmergencia);
-        return response.status(201).json(profile);
-    } catch (error) {
-        return response.status(404).json({ message: 'Error al modificar el perfil', error: error.message });
+exports.patchPerfil = [
+    async (request, response) => {
+        const firebaseUID = request.userUID.uid;
+        const username = request.body.username;
+        const nombre = request.body.nombre
+        const tipoSangre = request.body.tipoSangre;
+        const numeroEmergencia = request.body.numeroEmergencia;
+
+        try {
+            const profile = await Usuario.patchPerfil(firebaseUID, username, nombre, tipoSangre, numeroEmergencia);
+            return response.status(201).json(profile);
+        } catch (error) {
+            return response.status(404).json({ message: 'Error al modificar perfil', error: error.message });
+        }
     }
-}
-
-
-
+];
