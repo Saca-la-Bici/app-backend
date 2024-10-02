06e44e27e60136ec871b4c0f9eabdba3bb7cbf54
Jesus0204 <a01705442@tec.mx>
2024-10-02 16:03:13 -0600

    fix: agregar token a ruta

--- modules/actividades/routes/registrarActividad.routes.js
+++ modules/actividades/routes/registrarActividad.routes.js
@@ -6,7 +6,7 @@ const verifyUserToken = require('../../../util/verifyUserToken')
 const registrarActividadController = require('../controllers/registrarActividad.controller');
 
 // Definir la ruta para crear una actividad
-router.post('/rodada', registrarActividadController.postRodada);
+router.post('/rodada', verifyUserToken, registrarActividadController.postRodada);
 router.post('/taller', verifyUserToken, registrarActividadController.postTaller);
 router.post('/evento', verifyUserToken, registrarActividadController.postEvento);
 
