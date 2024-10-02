8cdc76933e6869f6a92d671d1ee89774503c32b6
Angeltrek <angeltrek007@gmail.com>
2024-10-01 19:34:36 -0600

    feat: consultar usuarios con imagen

--- modules/perfil/controllers/consultarUsuarios.controller.js
+++ modules/perfil/controllers/consultarUsuarios.controller.js
@@ -1,70 +1,50 @@
 const PoseeRol = require("../../../models/perfil/poseeRol.model");
 const Usuario = require("../../../models/perfil/usuario.model").Usuario;
 const Rol = require("../../../models/perfil/rol.model");
-const getImageFolder = require("../../../util/getImageFolder");
+const getUserImage = require("../../../util/getUserImage");
 
 exports.getUsuarios = async (req, res) => {
   const { page, limit, roles } = req.query;
-
-  // Obtener el firebaseUID del usuario autenticado
   const firebaseUID = req.userUID.uid;
-  const folder = "profile/";
 
   try {
-    // Verificar que los valores de page y limit sean números válidos
-    const pageInt = parseInt(page);
-    const limitInt = parseInt(limit);
+    const pageInt = isNaN(parseInt(page)) ? 1 : parseInt(page);
+    const limitInt = isNaN(parseInt(limit)) ? 10 : parseInt(limit);
 
-    if (isNaN(pageInt) || isNaN(limitInt) || pageInt < 1 || limitInt < 1) {
+    if (pageInt < 1 || limitInt < 1) {
       return res
         .status(400)
         .json({ message: "Parámetros de paginación inválidos" });
     }
 
-    // Obtener el array de roles de la query (puede ser 'Administrador,Usuario' o 'Staff,Usuario')
     const rolesArray = roles
       ? roles.split(",")
       : ["Administrador", "Staff", "Usuario"];
+    const rolEspecial = rolesArray[0];
+    const rolNormal = "Usuario";
 
-    // Roles especiales (Administrador o Staff) y el rol normal (Usuario)
-    const rolEspecial = rolesArray[0]; // El primer rol en la query ya sea Administrador o Staff
-    const rolNormal = "Usuario"; // El segundo rol siempre es "Usuario"
-
-    // Buscar el ObjectId del rol basado en el nombre
     const rolEspecialDoc = await Rol.findOne({ nombre: rolEspecial }).select(
       "_id"
     );
-    if (!rolEspecialDoc) {
+    if (!rolEspecialDoc)
       return res.status(404).json({ message: "Rol no encontrado" });
-    }
-    const rolEspecialID = rolEspecialDoc._id;
 
-    // Buscar el ObjectId del usuario basado en firebaseUID
-    const usuario = await Usuario.findOne({ firebaseUID: firebaseUID }).select(
-      "_id"
-    );
-
-    if (!usuario) {
+    const usuario = await Usuario.findOne({ firebaseUID }).select("_id");
+    if (!usuario)
       return res.status(404).json({ message: "Usuario no encontrado" });
-    }
 
     const usuarioID = usuario._id;
 
-    // Contar el total de documentos para los roles especiales
     const totalEspeciales = await PoseeRol.countDocuments({
-      IDRol: rolEspecialID, // Usar el ObjectId aquí
+      IDRol: rolEspecialDoc._id,
       IDUsuario: { $ne: usuarioID },
     });
 
-    // Consultar usuarios con el rol especial (Administrador o Staff)
     let usuariosEspeciales = await PoseeRol.find({
-      IDRol: rolEspecialID, // Usar el ObjectId aquí
+      IDRol: rolEspecialDoc._id,
       IDUsuario: { $ne: usuarioID },
     })
-      .populate({
-        path: "IDRol",
-        select: "nombre",
-      })
+      .populate({ path: "IDRol", select: "nombre" })
       .populate({
         path: "IDUsuario",
         match: { _id: { $ne: usuarioID } },
@@ -75,28 +55,21 @@ exports.getUsuarios = async (req, res) => {
       .limit(limitInt)
       .lean();
 
-    // Si no hay más usuarios con el rol especial o la cantidad de usuarios especiales es menor al límite, cargar los usuarios normales
     const remainingLimit = limitInt - usuariosEspeciales.length;
 
     let usuariosNormales = [];
     if (remainingLimit > 0) {
-      // Obtener el ObjectId del rol 'Usuario'
       const rolNormalDoc = await Rol.findOne({ nombre: rolNormal }).select(
         "_id"
       );
-      if (!rolNormalDoc) {
+      if (!rolNormalDoc)
         return res.status(404).json({ message: "Rol no encontrado" });
-      }
-      const rolNormalID = rolNormalDoc._id;
 
       usuariosNormales = await PoseeRol.find({
-        IDRol: rolNormalID,
+        IDRol: rolNormalDoc._id,
         IDUsuario: { $ne: usuarioID },
       })
-        .populate({
-          path: "IDRol",
-          select: "nombre",
-        })
+        .populate({ path: "IDRol", select: "nombre" })
         .populate({
           path: "IDUsuario",
           match: { _id: { $ne: usuarioID } },
@@ -108,30 +81,23 @@ exports.getUsuarios = async (req, res) => {
         .lean();
     }
 
-    // Combinar los resultados
-    const usuariosConRoles = [...usuariosEspeciales, ...usuariosNormales];
+    const usuariosConRoles = [
+      ...usuariosEspeciales,
+      ...usuariosNormales,
+    ].filter((ur) => ur.IDUsuario && ur.IDRol);
 
-    const usuariosFiltrados = usuariosConRoles.filter(
-      (ur) => ur.IDUsuario && ur.IDRol
-    );
-
-    // Mapeamos los resultados y manejamos la imagen solo si es necesario
     const result = await Promise.all(
-      usuariosFiltrados.map(async (ur) => {
-        // Verificamos si el usuario tiene una imagen asociada
-        if (ur.IDUsuario.imagen) {
-          console.log(ur.IDUsuario.imagen);
-          ur.IDUsuario.imagen = await getImageFolder(req, folder); // Obtenemos la imagen solo si existe
-          console.log(ur.IDUsuario.imagen);
-        }
-
+      usuariosConRoles.map(async (ur) => {
+        const imagenPerfil = ur.IDUsuario.imagen
+          ? await getUserImage(ur.IDUsuario._id, ur.IDUsuario.imagen)
+          : null;
         return {
           usuario: {
             id: ur.IDUsuario._id,
             username: ur.IDUsuario.username,
             nombre: ur.IDUsuario.nombre,
             correoElectronico: ur.IDUsuario.correoElectronico,
-            imagenPerfil: ur.IDUsuario.imagen || null, // Será null si no tiene imagen
+            imagenPerfil: imagenPerfil || null,
           },
           rol: {
             id: ur.IDRol._id,
@@ -141,6 +107,10 @@ exports.getUsuarios = async (req, res) => {
       })
     );
 
+    if (result.length === 0) {
+      return res.status(404).json({ message: "No se encontraron usuarios." });
+    }
+
     res.status(200).json({
       usuarios: result,
       currentPage: pageInt,
@@ -148,8 +118,10 @@ exports.getUsuarios = async (req, res) => {
       totalPages: Math.ceil(totalEspeciales / limitInt),
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error al obtener los usuarios", error: error.message });
+    console.error("Error en getUsuarios:", error);
+    res.status(500).json({
+      message: "Error al obtener los usuarios",
+      error: error.toString(),
+    });
   }
 };
