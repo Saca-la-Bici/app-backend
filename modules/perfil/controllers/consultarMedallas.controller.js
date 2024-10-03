59b0ca3f5517b0727f52c376beecc6406103758b
PAOLA MARIA garrido <you@example.com>
2024-10-03 17:00:47 -0600

    feat: Agregar medalla model, controller y route

--- modules/perfil/controllers/consultarMedallas.controller.js
+++ modules/perfil/controllers/consultarMedallas.controller.js
@@ -1,11 +1,12 @@
 const Medalla = require('../../../models/medallas/medalla.model');
 
-exports.consultarMedallas = async (req, res) => {
+exports.getMedallas = async (req, res) => {
     try {
         const medallas = await Medalla.find();
         return res.status(200).json({
             code: 200,
             msg: 'Consulta de medallas exitosa',
+            permisos: req.permisos,
             data: medallas
         });
     } catch (error) {
@@ -13,6 +14,7 @@ exports.consultarMedallas = async (req, res) => {
         return res.status(500).json({
             code: 500,
             msg: 'Error al intentar consultar las medallas',
+            permisos: req.permisos,
             data: null
         });
     }
