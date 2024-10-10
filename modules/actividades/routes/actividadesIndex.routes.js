3f731ab567c8380301f3d50e584f5e2326fac8f4
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-10-09 19:20:42 -0600

    feat: eliminar actividad y limpiar código

--- modules/actividades/routes/actividadesIndex.routes.js
+++ modules/actividades/routes/actividadesIndex.routes.js
@@ -6,6 +6,7 @@ const modificarActividadRoute = require('./modificarActividad.routes');
 const consultarActividadesRoute = require('./consultarActividades.routes');
 const inscripcionActividadRoute = require('./inscribirActividad.routes');// Nueva ruta de inscripción
 const cancelarAsistenciaRoute = require('./cancelarAsistencia.routes');
+const eliminarActividadRoute = require('./eliminarActividad.routes');
 
 // Importar y usar las rutas en el enrutador principal del módulo
 router.use('/registrar', registrarActividadRoute);
@@ -13,5 +14,6 @@ router.use('/modificar', modificarActividadRoute);
 router.use('/consultar', consultarActividadesRoute);
 router.use('/inscripcion', inscripcionActividadRoute); // ruta de inscripción
 router.use('/cancelarAsistencia', cancelarAsistenciaRoute);
+router.use('/eliminar', eliminarActividadRoute);
 
 module.exports = router;
