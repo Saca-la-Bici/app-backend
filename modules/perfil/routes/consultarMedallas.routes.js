a05bef9929775c3b5b6185043a334d70b503b220
PAOLA MARIA garrido <you@example.com>
2024-10-10 18:26:32 -0600

    feat: Ajustar routes de consultar medallas

--- modules/perfil/routes/consultarMedallas.routes.js
+++ modules/perfil/routes/consultarMedallas.routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-
-const verifyUserToken = require('../../../util/verifyUserToken');
-const verifyUserPermissions = require('../../../util/verifyUserPermissions');
-const { consultarMedallas } = require("../controllers/consultarMedallas.controller");
-
+// Importar el controlador que maneja la creación de medallas
+const consultarMedallasController = require("../controllers/consultarMedallas.controller");
+const verifyUserToken = require("../../../util/verifyUserToken");
 
 // Definir la ruta para crear una medalla
-router.get("/", verifyUserToken, verifyUserPermissions, consultarMedallas);
+router.get("/",verifyUserToken, consultarMedallasController.consultarMedallas);
 
-module.exports = router;
+module.exports = router;
