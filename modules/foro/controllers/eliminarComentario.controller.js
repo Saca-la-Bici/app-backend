27fcb3ecf5cfc0424be25d6326ecd0742152b30b
AntonioLanderos <a00574000@tec.mx>
2024-10-14 10:06:32 -0600

    refactor(eliminarComentario): cambiar a controller actualizado

--- modules/foro/controllers/eliminarComentario.controller.js
+++ modules/foro/controllers/eliminarComentario.controller.js
@@ -1,17 +1,18 @@
-const Comentario = require('../../../models/foro/comentario.model');
+const Comentario = require('../../../models/foro/foro.model');
+const Foro = require('../../../models/foro/foro.model');
 
-exports.eliminarComentario = async (request, response) => {
-    const idComentario = request.params.idComentario;
-   
+exports.eliminarComentario = async (req, res) => {
+    const { id } = req.params; 
+
     try {
-        const comentario = await Comentario.eliminarComentario(idComentario);
-        
-        if (!comentario) {
-            return response.status(404).json({ message: 'Comentario no encontrado' });
-        }
-        
-        return response.status(200).json({ message: 'Comentario eliminado correctamente' });
+        const comentarioEliminado = await Comentario.findByIdAndDelete(id);
+        await Foro.updateMany(
+            {},
+            { $pull: { comentarios: id } }  
+        );
+
+        res.status(200).json(comentarioEliminado);
     } catch (error) {
-        return response.status(500).json({ message: 'Error al eliminar el comentario', error: error.message });
+        res.status(500).json({ message: 'Error al eliminar el comentario', error: error.message });
     }
 };
