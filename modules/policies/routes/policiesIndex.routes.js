51c00964fe2d33f5ed444135ec08df11317f4311
Jesus0204 <a01705442@tec.mx>
2024-10-14 23:03:51 -0600

    feat: agregar routes y controlador para terminos y condiciones

--- modules/policies/routes/policiesIndex.routes.js
+++ modules/policies/routes/policiesIndex.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const privacyPolicyRoute = require('./privacyPolicy.routes');
+const termsConditionsRoute = require('./termsConditions.routes');
 
 // Importar y usar las rutas en el enrutador principal del módulo
 router.use('/politicaPrivacidad', privacyPolicyRoute);
+router.use('/terminosCondiciones', termsConditionsRoute);
 
 module.exports = router;
