b4088daa837ce22394aad6fd13bbec97aa5e754b
A01710217 <a01710217@tec.mx>
2024-10-16 00:48:39 -0600

    fix(cronjobKmMes): arreglar hora de cambio KmMes

--- util/cronjobKmMes.js
+++ util/cronjobKmMes.js
@@ -3,7 +3,7 @@ const cron = require("node-cron");
 
 // Función para programar la tarea de cron que cambia el estado de la alerta
 exports.start = () => {
-  // Tarea programada para ejecutarse el primer día de cada mes a las 23:35
+  // Tarea programada para ejecutarse el primer día de cada mes a las 00:01
   cron.schedule("1 0 1 * *", async () => {
     try {
       // Reestablecer el campo kilometrosMes a 0 para todos los usuarios
