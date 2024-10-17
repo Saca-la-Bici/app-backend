eb12de473786dc21d9620d04d1a259037f380c66
DanielQueijeiro <dany.queijeiro@hotmail.com>
2024-10-17 08:11:12 -0600

    <style> Comentarios

--- modules/anuncios/controllers/registrarAnuncio.controller.js
+++ modules/anuncios/controllers/registrarAnuncio.controller.js
@@ -1,21 +1,30 @@
+// Importa el modelo de anuncios y las funciones para subir y eliminar imágenes
 const Announcement = require("../../../models/otros/anuncio.model");
 const { upload, uploadToS3 } = require("../../../util/uploadImage");
 const { Usuario } = require("../../../models/perfil/usuario.model");
 
+// Importar función para enviar notificaciones
 const sendNotification = require("../../../util/sendNotification");
 
+// Define la carpeta de imágenes para los anuncios
 const folder = "announcements/";
 
 exports.postAnnouncement = [
+  // Middleware para manejar la subida de una imagen al S3
   upload.single("file"),
   uploadToS3(folder),
+
   async (request, response) => {
+    // Obtener los parámetros de la solicitud
     const firebaseUID = request.userUID.uid;
     const titulo = request.body.titulo;
     const contenido = request.body.contenido;
+
+    // Obtiene el nombre del archivo de la imagen si existe
     const imagen = request.file ? request.file.filename : null;
 
     try {
+      // Crear un nuevo anuncio con los parámetros de la solicitud
       const announcement = await Announcement.postAnnouncement(
         firebaseUID,
         titulo,
@@ -43,10 +52,11 @@ exports.postAnnouncement = [
       await sendNotification(tokens, tituloNotificacion, cuerpoNotificacion, {
         anuncioID: announcement._id.toString(),
       });
-
+      // Responder con un estado 201 y un mensaje de éxito
       return response
         .status(201)
         .json({ message: "Anuncio creado exitosamente" });
+      // En caso de error, imprimir el error en la consola y responder con un estado 500
     } catch (error) {
       console.error("Error in postAnnouncement:", error);
       return response
